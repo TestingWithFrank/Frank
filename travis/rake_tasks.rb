c0700cf3495a2c79b00db48df48c009c651837f0
Pete Hodgson <git@thepete.net>
2013-05-10 16:17:36 -0700

    add local frank gem bin dir to $PATH when running ci

--- travis/rake_tasks.rb
+++ travis/rake_tasks.rb
@@ -2,13 +2,14 @@ require 'pathname'
 ROOT_DIR = Pathname.new(__FILE__).dirname.join("..").realpath
 CONTROLS_APP_DIR = ROOT_DIR.join("example").join("Controls")
 GEM_LIB_DIR = ROOT_DIR.join("gem").join("lib")
+GEM_BIN_DIR = ROOT_DIR.join("gem").join("bin")
 
 def in_controls_app(&block)
   Dir.chdir( CONTROLS_APP_DIR, &block )
 end
 
 def sh_using_local_frank_gem cmd
-  full_cmd = "(export RUBYLIB='#{GEM_LIB_DIR}'; #{cmd})"
+  full_cmd = "(export RUBYLIB='#{GEM_LIB_DIR}' PATH='#{GEM_LIB_DIR}':$PATH; #{cmd})"
   sh full_cmd
 end
 
@@ -19,8 +20,9 @@ namespace :ci do
   namespace :example_app do
     task :build do
       in_controls_app do
-        sh_using_local_frank_gem "frank update"
-        sh_using_local_frank_gem "frank build"
+        sh_using_local_frank_gem "echo $PATH"
+        #sh_using_local_frank_gem "frank update"
+        #sh_using_local_frank_gem "frank build"
       end
     end
 
