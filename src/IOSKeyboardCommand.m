74ab637dd0e6c3798f62d33c87b456d43c2c358d
Michael Buckley <thebuckley@gmail.com>
2014-01-16 21:18:23 -0800

    Fix keyboard in portrait orientation

--- src/IOSKeyboardCommand.m
+++ src/IOSKeyboardCommand.m
@@ -4,12 +4,11 @@
 // To change the template use AppCode | Preferences | File Templates.
 //
 
-
+#import "FranklyProtocolHelper.h"
 #import "IOSKeyboardCommand.h"
 #import "JSON.h"
-#import <PublicAutomation/UIAutomationBridge.h>
-
-#import "FranklyProtocolHelper.h"
+#import "KIFTypist.h"
+#import "UIApplication-KIFAdditions.h"
 
 @implementation IOSKeyboardCommand {
 
@@ -25,11 +24,14 @@
     NSDictionary *requestCommand = FROM_JSON(requestBody);
 	NSString *textToType = [requestCommand objectForKey:@"text_to_type"];
 
-    if( ![UIAutomationBridge checkForKeyboard] ){
+    if( ![[UIApplication sharedApplication] keyboardWindow]){
         return [self generateKeyboardNotPresentErrorResponse];
     }
     
-    [UIAutomationBridge typeIntoKeyboard:textToType];
+    for (NSUInteger characterIndex = 0; characterIndex < [textToType length]; characterIndex++) {
+        NSString *characterString = [textToType substringWithRange:NSMakeRange(characterIndex, 1)];
+        [KIFTypist enterCharacter: characterString];
+    }
 
 	return [FranklyProtocolHelper generateSuccessResponseWithoutResults];
 }
